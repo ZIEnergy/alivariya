4aa34681a036fd2540ca10c95db3b6f0e994af52
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-16 16:53:41 +0300

    add question screen

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,6 @@
-$(document).ready(function(){})
+$(document).ready(function(){
+
+
+
+
+$(function(){$(window).bind("resize",function(){resizeMe()}).trigger("resize")});var resizeMe=function(){var i=2550,e=10,n=$(window).width(),r=n/i,s=e*r;$("html").css("font-size",s)};})
