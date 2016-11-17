79bd59d007a4465e17760e92b4df4777f6c44e62
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-17 13:56:09 +0300

    add few objects

--- build/js/script.js
+++ build/js/script.js
@@ -5,4 +5,5 @@ $(document).ready(function(){
 
 $(".question__button--yes").click(function(t){t.preventDefault(),$(".question").fadeOut("fast")});
 
+
 $(function(){$(window).bind("resize",function(){resizeMe()}).trigger("resize")});var resizeMe=function(){var i=2550,e=10,n=$(window).width(),r=n/i,s=e*r;$("html").css("font-size",s)};})
