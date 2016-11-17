b52fecde211b987ca8078584c4362c2da91a37a4
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-17 21:49:56 +0300

    add location content

--- build/js/script.js
+++ build/js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
 
+$(".card__button").click(function(t){t.preventDefault(),$(".front").hide(),$(".game").fadeIn("fast")});
+
+
 
 
 $(".question__button--yes").click(function(t){t.preventDefault(),$(".question").fadeOut("fast")});
+
 
 
 $(function(){$(window).bind("resize",function(){resizeMe()}).trigger("resize")});var resizeMe=function(){var i=2550,e=10,n=$(window).width(),r=n/i,s=e*r;$("html").css("font-size",s)};})
---
+++ src/blocks/front/front.js
@@ -0,0 +1,5 @@
+$('.card__button').click(function(e) {
+  e.preventDefault();
+  $('.front').hide();
+  $('.game').fadeIn('fast');
+});
