1ca3074fb50489af350b8366fbb098b371d90bbe
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-19 11:57:10 +0300

    finished bgs try

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-$(".card-game__button").click(function(a){a.preventDefault(),$(".card-game").fadeOut("fast")});
 
+$(".card-game__button").click(function(a){a.preventDefault(),$(".card-game").fadeOut("fast")});
 $(".card__button").click(function(t){t.preventDefault(),$(".front").hide(),$(".game").fadeIn("fast")});
 $(".game__ingredient").click(function(e){e.preventDefault(),$(".game-popup__picture").attr("src","img/"+$(this).data("picture")),$(".game-popup__header").text($(this).data("header")),$(".game-popup__text").text($(this).data("text")),$(".game-popup").fadeIn("fast").css("display","flex")}),$(".game__ingredient--correct").click(function(e){e.preventDefault(),$(".game-popup__status").removeClass("game-popup__status--minus"),$(".game-popup__status").addClass("game-popup__status--key")}),$(".game__ingredient--incorrect").click(function(e){e.preventDefault(),$(".game-popup__status").removeClass("game-popup__status--key"),$(".game-popup__status").addClass("game-popup__status--minus")}),$(".game-popup__close").click(function(e){e.preventDefault(),$(".game-popup").fadeOut("fast")});
 
